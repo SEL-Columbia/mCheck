46963678f521afd0849b80064d20ba21c2e854df
Kiran <kirang20@gmail.com>
2013-08-28 15:14:10 +0530

    Kiran | #4 | Now the mother is enrolled to the milestone based on registration date.

--- mcheck-core/src/main/java/org/who/mcheck/core/service/MotherScheduleService.java
+++ mcheck-core/src/main/java/org/who/mcheck/core/service/MotherScheduleService.java
@@ -2,13 +2,20 @@ package org.who.mcheck.core.service;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.joda.time.LocalDate;
 import org.joda.time.LocalTime;
 import org.motechproject.model.Time;
+import org.motechproject.scheduletracking.api.domain.Milestone;
+import org.motechproject.scheduletracking.api.domain.Schedule;
+import org.motechproject.scheduletracking.api.repository.AllSchedules;
 import org.motechproject.scheduletracking.api.service.EnrollmentRequest;
 import org.motechproject.scheduletracking.api.service.ScheduleTrackingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.who.mcheck.core.util.DateUtil;
+
+import java.text.MessageFormat;
 
 import static org.joda.time.LocalDate.parse;
 import static org.who.mcheck.core.AllConstants.Schedule.POST_DELIVERY_DANGER_SIGNS_SCHEDULE_NAME;
@@ -16,25 +23,40 @@ import static org.who.mcheck.core.AllConstants.Schedule.POST_DELIVERY_DANGER_SIG
 @Service
 public class MotherScheduleService {
     private final Log log = LogFactory.getLog(MotherScheduleService.class);
-
+    private AllSchedules allSchedules;
     private ScheduleTrackingService scheduleTrackingService;
     private final String preferredCallTime;
 
     @Autowired
     public MotherScheduleService(ScheduleTrackingService scheduleTrackingService,
+                                 AllSchedules allSchedules,
                                  @Value("#{mCheck['ivr.preferred.call.time']}") String preferredCallTime) {
         this.scheduleTrackingService = scheduleTrackingService;
+        this.allSchedules = allSchedules;
         this.preferredCallTime = preferredCallTime;
     }
 
     public void enroll(String motherId, String registrationDate) {
+        String startingMilestoneName = getStartingMilestoneName(POST_DELIVERY_DANGER_SIGNS_SCHEDULE_NAME, parse(registrationDate));
         EnrollmentRequest scheduleEnrollmentRequest = new EnrollmentRequest()
                 .setScheduleName(POST_DELIVERY_DANGER_SIGNS_SCHEDULE_NAME)
                 .setExternalId(motherId)
                 .setPreferredAlertTime(new Time(LocalTime.parse(preferredCallTime)))
-                .setReferenceDate(parse(registrationDate));
+                .setReferenceDate(parse(registrationDate))
+                .setStartingMilestoneName(
+                        startingMilestoneName);
 
-        log.info("Enrolling mother with ID: " + motherId + " to schedule: " + POST_DELIVERY_DANGER_SIGNS_SCHEDULE_NAME + " preferred call time: " + preferredCallTime);
+        log.info(MessageFormat.format("Enrolling mother with ID: {0} to schedule: {1}, to milestone: {2} preferred call time: {3}",
+                motherId, POST_DELIVERY_DANGER_SIGNS_SCHEDULE_NAME, startingMilestoneName, preferredCallTime));
         scheduleTrackingService.enroll(scheduleEnrollmentRequest);
+    }
+
+    private String getStartingMilestoneName(String name, LocalDate referenceDate) {
+        Schedule schedule = allSchedules.getByName(name);
+        for (Milestone milestone : schedule.getMilestones()) {
+            if (referenceDate.plus(milestone.getMaximumDuration()).isAfter(DateUtil.today()))
+                return milestone.getName();
+        }
+        return null;
     }
 }
--- mcheck-core/src/test/java/org/who/mcheck/core/service/MotherScheduleServiceTest.java
+++ mcheck-core/src/test/java/org/who/mcheck/core/service/MotherScheduleServiceTest.java
@@ -1,21 +1,29 @@
 package org.who.mcheck.core.service;
 
 import org.joda.time.LocalDate;
+import org.joda.time.Period;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentMatcher;
 import org.mockito.Mock;
+import org.motechproject.scheduletracking.api.domain.Milestone;
+import org.motechproject.scheduletracking.api.domain.Schedule;
+import org.motechproject.scheduletracking.api.repository.AllSchedules;
 import org.motechproject.scheduletracking.api.service.EnrollmentRequest;
 import org.motechproject.scheduletracking.api.service.ScheduleTrackingService;
+import org.who.mcheck.core.util.DateUtil;
 
 import static org.joda.time.LocalDate.parse;
 import static org.mockito.Matchers.argThat;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 public class MotherScheduleServiceTest {
     @Mock
     private ScheduleTrackingService scheduleTrackingService;
+    @Mock
+    private AllSchedules allSchedules;
 
     @Before
     public void setUp() throws Exception {
@@ -24,20 +32,38 @@ public class MotherScheduleServiceTest {
 
     @Test
     public void shouldEnrollMotherToPostPregnancyDangerSignsSchedule() throws Exception {
-        MotherScheduleService service = new MotherScheduleService(scheduleTrackingService, "14:00:00");
-        service.enroll("id", "2013-01-01");
+        MotherScheduleService service = new MotherScheduleService(scheduleTrackingService, allSchedules, "14:00:00");
+        Milestone firstMilestone = new Milestone("Day1", days(0), days(0), days(0), days(1));
+        Milestone secondMilestone = new Milestone("Day2", days(2), days(2), days(2), days(2));
+        Schedule schedule = new Schedule("Post Delivery Danger Signs");
+        schedule.addMilestones(firstMilestone);
+        schedule.addMilestones(secondMilestone);
 
-        verify(scheduleTrackingService).enroll(enrollmentFor("id", "Post Delivery Danger Signs", parse("2013-01-01")));
+        when(allSchedules.getByName("Post Delivery Danger Signs")).thenReturn(schedule);
+
+        DateUtil.fakeIt(parse("2013-01-01"));
+        service.enroll("id", "2013-01-01");
+        verify(scheduleTrackingService).enroll(enrollmentFor("id", "Post Delivery Danger Signs", parse("2013-01-01"), "Day1"));
+
+        DateUtil.fakeIt(parse("2013-01-02"));
+        service.enroll("id", "2013-01-01");
+        verify(scheduleTrackingService).enroll(enrollmentFor("id", "Post Delivery Danger Signs", parse("2013-01-01"), "Day2"));
     }
 
-    private EnrollmentRequest enrollmentFor(final String externalId, final String scheduleName, final LocalDate referenceDate) {
+    private EnrollmentRequest enrollmentFor(final String externalId, final String scheduleName,
+                                            final LocalDate referenceDate, final String startingMilestone) {
         return argThat(new ArgumentMatcher<EnrollmentRequest>() {
             @Override
             public boolean matches(Object o) {
                 EnrollmentRequest request = (EnrollmentRequest) o;
                 return externalId.equals(request.getExternalId()) && referenceDate.equals(request.getReferenceDate())
-                        && scheduleName.equals(request.getScheduleName());
+                        && scheduleName.equals(request.getScheduleName())
+                        && startingMilestone.equals(request.getStartingMilestoneName());
             }
         });
+    }
+
+    private Period days(int numberOfDays) {
+        return new Period(0, 0, 0, numberOfDays, 0, 0, 0, 0);
     }
 }
