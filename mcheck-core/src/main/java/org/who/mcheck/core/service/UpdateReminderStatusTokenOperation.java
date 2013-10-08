863df3467de5a6a82519c17d3da2abb1fe89abe8
Kiran <kirang20@gmail.com>
2013-10-08 22:41:56 +0530

    Kiran | #21 | Refactoring.

--- mcheck-core/src/main/java/org/who/mcheck/core/service/UpdateReminderStatusTokenOperation.java
+++ mcheck-core/src/main/java/org/who/mcheck/core/service/UpdateReminderStatusTokenOperation.java
@@ -6,10 +6,8 @@ import org.apache.commons.logging.LogFactory;
 import org.motechproject.decisiontree.core.FlowSession;
 import org.motechproject.decisiontree.core.model.INodeOperation;
 import org.motechproject.decisiontree.server.domain.FlowSessionRecord;
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 import org.who.mcheck.core.ApplicationContextUtils;
 import org.who.mcheck.core.domain.ReminderStatusToken;
@@ -21,14 +19,15 @@ import static java.text.MessageFormat.format;
 import static org.apache.commons.lang.exception.ExceptionUtils.getFullStackTrace;
 
 @Component
-public class UpdateReminderStatusTokenOperation implements INodeOperation, ApplicationContextAware {
+public class UpdateReminderStatusTokenOperation implements INodeOperation {
 
     private final Log log = LogFactory.getLog(UpdateReminderStatusTokenOperation.class);
     private AllReminderStatusTokens allReminderStatusTokens;
-    private ApplicationContext applicationContext;
 
     public UpdateReminderStatusTokenOperation() {
-        applicationContext = ApplicationContextUtils.getApplicationContext();
+        ApplicationContext applicationContext = ApplicationContextUtils.getApplicationContext();
+        log.info("AllReminderStatusTokens is null, so autowiring it");
+        allReminderStatusTokens = applicationContext.getBean(AllReminderStatusTokens.class);
     }
 
     @Autowired
@@ -39,20 +38,6 @@ public class UpdateReminderStatusTokenOperation implements INodeOperation, Appli
     @Override
     public void perform(String userInput, FlowSession session) {
         try {
-            if (allReminderStatusTokens == null) {
-                log.info("AllReminderStatusTokens is null, so autowiring it");
-                allReminderStatusTokens = applicationContext.getBean(AllReminderStatusTokens.class);
-                if (allReminderStatusTokens == null) {
-                    log.error("Autowiring AllReminderStatusTokens. Second try!");
-                    allReminderStatusTokens = (AllReminderStatusTokens) applicationContext.getBean(Class.forName("org.who.mcheck.core.repository.AllReminderStatusTokens"));
-                }
-                if (allReminderStatusTokens == null) {
-                    log.error("Could not autowire AllReminderStatusTokens!");
-                }
-            }
-            if (applicationContext == null) {
-                log.error("Spring application context is also null!");
-            }
             ReminderStatusToken token = allReminderStatusTokens.findByContactNumber(session.getPhoneNumber());
             if (token == null) {
                 log.warn(format("Could not find a call status token for the phone number: {0}. Flow session: {1}",
@@ -69,10 +54,5 @@ public class UpdateReminderStatusTokenOperation implements INodeOperation, Appli
             log.error(MessageFormat.format("Error when executing UpdateReminderStatusTokenOperation. Message: {0}, Stack trace: {1}",
                     e.getMessage(), getFullStackTrace(e)));
         }
-    }
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext = applicationContext;
     }
 }
